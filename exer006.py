47a22da9117ee7cf82eacdfef839adbcca49093a
wesllanSilva <62728922+wesllanSilva@users.noreply.github.com>
2020-11-01 10:47:31 -0300

    Create exer006.py
    programa que mostre o dobro o triplo e a raiz quadrada de um número.

---
+++ exer006.py
@@ -0,0 +1,7 @@
+#DOBRRO, TRIPLO E RAIZ QUADRADA.
+n = int(input("DIGITE UM NÚMERO: "))
+d = 2 * n
+t = 3 * n
+r = n **(1/2)
+
+print("O dobro de {} é {} \nO triplo de {} é {} \nA raiz quadrada de {} é {:.2f}".format(n,d,n,t,n,r))
