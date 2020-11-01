77b4e797b82432d12a3a7b7f91dea5a39ea3f8e2
wesllanSilva <62728922+wesllanSilva@users.noreply.github.com>
2020-11-01 11:05:06 -0300

    Create exer007.py
    programa que calcula a média aritimética entre dois números

---
+++ exer007.py
@@ -0,0 +1,5 @@
+#MÉDIA ARITIMÉTICA.
+n1 = float(input("DIGITE A PRIMEIRA NOTA: "))
+n2 = float(input("DIGITE A SEGUNDA NOTA: "))
+m = (n1+n2)/2
+print("A média aritimética entre {} e {} é {:.2f}".format(n1,n2,m))
