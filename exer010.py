aca04cb28700cabae68a5d0eda497f1025ddd8ed
wesllanSilva <62728922+wesllanSilva@users.noreply.github.com>
2020-11-01 21:34:06 -0300

    Update exer010.py
    atualizado o ponto flutuante para 2 casas depois da virgula

--- exer010.py
+++ exer010.py
@@ -1,4 +1,4 @@
 #CONVERSOR DE MEDIDAS.
 r = float(input("Quanto dinheiro você tem? R$ "))
 d = r / 5.74
-print("Com R${} você pode comprar US${:.3f}".format(r,d))
+print("Com R${:.2f} você pode comprar US${:.2f}".format(r,d))
