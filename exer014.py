7f9bedd9d6983685d2adfa80c4bb33efd494931b
wesllanSilva <62728922+wesllanSilva@users.noreply.github.com>
2020-11-01 23:48:08 -0300

    Create exer014.py
    programa que converte uma temperatura digitada em graus Celsius  para graus Fahrenheit

---
+++ exer014.py
@@ -0,0 +1,4 @@
+#CONVERSOR DE TEMPERATURAS.
+celcius = float(input("Informe a temperatura em ºC: "))
+fahrenheit = celcius * 1.8 + 32
+print("A temperatura de {}ºC corresponde a {}ºF".format(celcius,fahrenheit))
