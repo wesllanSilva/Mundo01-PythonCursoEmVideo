433fc403884c532826d6b001f141061ed8b0f149
wesllanSilva <wesllan90@gmail.com>
2021-09-15 13:36:07 -0300

    programa para aprovar o empréstimo bancário para a compra de uma casa. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.

---
+++ Mundo02/exer036.py
@@ -0,0 +1,12 @@
+#Aprovando Emprestimo!
+valor_casa = float(input("Valor da casa: R$"))
+salario = float(input('Salario do comprador: R$'))
+anos = int(input('Quantos anos de financiamento: R$'))
+parcela = valor_casa / (anos * 12)
+dinheiro = salario * 30/100
+
+print('Para pagar uma casa de R${}  em {} anos a parcela vai ter o valor de R${:.2f}.'.format(valor_casa,anos,parcela))
+if parcela > dinheiro:
+    print('Empréstimo NEGADO!')
+else:
+    print('Emprestimo APROVADO!')
