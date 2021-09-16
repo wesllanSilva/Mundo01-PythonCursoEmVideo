857a1d3a3962cd91f3f20f8a3bebdaccf0ebc780
wesllanSilva <wesllan90@gmail.com>
2021-09-16 14:35:40 -0300

    Programa que leia dois número inteiro e mostre se o primeiro ou segundo é maior ou se são iguais.

---
+++ Mundo02/38-ComparandoNumeros.py
@@ -0,0 +1,9 @@
+#COMPARANDO NUMEROS.
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+if n1 > n2:
+    print("o Primeiro valor é maior que o segundo.")
+elif n1 < n2:
+    print('O segundo valor é maior que o primeiro')
+else:
+    print('Nenhum valor é maior ou menor, os dois são iguais')
