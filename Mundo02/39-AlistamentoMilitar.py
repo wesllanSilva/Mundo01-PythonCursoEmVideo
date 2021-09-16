ef8372907ac429d335a33817e1338255a98739a3
wesllanSilva <wesllan90@gmail.com>
2021-09-16 17:05:57 -0300

    programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar

--- Mundo02/39-AlistamentoMilitar.py
+++ Mundo02/39-AlistamentoMilitar.py
@@ -9,8 +9,12 @@ if idade == 18:
     print('Você tem que se alistar neste ano.')
 elif idade < 18:
     diferenca = 18 - idade
+    ano = dataHoje + diferenca
     print('Para você fazer o alistamento faltam {} anos.'.format(diferenca))
+    print('Seu alistamento será em {}'.format(ano))
 else:
     diferenca = idade - 18
+    ano = dataHoje - diferenca
     print('Você deveria ter se alistado há {} anos.'.format(diferenca))
+    print('Seu alistamento foi em {}'.format(ano))
 
